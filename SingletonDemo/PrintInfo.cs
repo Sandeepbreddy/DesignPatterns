304aef67252d030115e1ac0796861131747e46ef
Sandeep Reddy <bsreddy0600@gmail.com>
2020-02-22 20:24:50 -0600

    Sample Singleton Console Example

---
+++ SingletonDemo/PrintInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SingletonDemo
+{
+    /// <summary>
+    ///    This is a Singleton Class which prints info. A Sample Demo class
+    ///    Should be sealed though the construcor is private because nested classes can ruin singleton principle
+    /// </summary>
+    public sealed class PrintInfo
+    {
+        private static int counter = 0;
+
+        //Lazy Initializatiion -- Takes thread safety internally 
+        //private static readonly Lazy<PrintInfo> instance = new Lazy<PrintInfo>(()=>new PrintInfo());
+        //public static PrintInfo GetInstance => instance.Value;
+
+        //Dynamic Initialization
+        public static PrintInfo GetInstance { get; } = new PrintInfo();
+
+        //This is previous way which is reduced to above opeartion
+
+        //private static readonly PrintInfo instance = new PrintInfo();
+        //public static PrintInfo GetInstance => instance;
+        //{
+        //    get
+        //    {
+        //        return instance.Value;
+        //    }
+        //}
+        private PrintInfo()
+        {
+            counter++;
+            Console.WriteLine("Counter value is " + counter.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating a parallel call
+
+            Parallel.Invoke(() => PrintInfo1(),
+                () => PrintInfo2());
+            // Calling a Class by creating an object
+
+            //Calling Client one
+            //Client1 c1 = new Client1();
+            //c1.printClient1("This is Client one Message to Print");
+
+            ////Calling Client two
+            //Client2 c2 = new Client2();
+            //c1.printClient1("This is Client two Message to Print");
+
+
+
+            Console.ReadLine();
+        }
+
+        private static void PrintInfo2()
+        {
+            PrintInfo pinfo1 = PrintInfo.GetInstance;
+            pinfo1.PrintDetails("This is Second Print Info");
+        }
+
+        private static void PrintInfo1()
+        {
+            PrintInfo pinfo = PrintInfo.GetInstance;
+            pinfo.PrintDetails("This is First Print Info");
+        }
+    }
+}
